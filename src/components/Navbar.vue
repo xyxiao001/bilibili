7e8d075836709a39725b78c47c34e79e5d08f805
xyxiao <463618622@qq.com>
2016-07-18 14:42:29 +0800

    add sass-loader and node-sass

---
+++ src/components/Navbar.vue
@@ -0,0 +1,35 @@
+<template lang="html">
+  <header>
+    <section>
+      <div class="logo">
+        <img src="../assets/recipe.png" alt="网站logo" />
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+    }
+  },
+  computed: {},
+  ready: function () {},
+  attached: function () {},
+  methods: {},
+  components: {}
+}
+</script>
+
+<style lang="scss">
+  header {
+    width: 100%;
+    height: 80px;
+
+    .logo {
+      margin-left: 3%;
+      margin-top: 5px;
+    }
+  }
+</style>
--- src/view/Dashboard.vue
+++ src/view/Dashboard.vue
@@ -1,3 +1,23 @@
-<template>
-  <p>首页</p>
+<template lang="html">
+  <Navbar></Navbar>
 </template>
+
+<script>
+import Navbar from '../components/Navbar'
+export default {
+  data: function () {
+    return {
+    }
+  },
+  computed: {},
+  ready: function () {},
+  attached: function () {},
+  methods: {},
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style lang="css">
+</style>
