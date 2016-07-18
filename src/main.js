5fc43b752c7d41a37a2ca89ab8caab586b10a7d0
xyxiao <463618622@qq.com>
2016-07-18 11:11:33 +0800

    添加路由配置

--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
-import App from './App'
+import VueRouter from 'vue-router'
+import App from './components/App'
 
-/* eslint-disable no-new */
-new Vue({
-  el: 'body',
-  components: { App }
+import Dashboard from './view/Dashboard'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  history: true,
+  linkActiveClass: 'active'
 })
+
+router.map({
+  '/': {
+    name: Dashboard,
+    component: Dashboard
+  }
+})
+
+router.redirect({
+  '*': '/'
+})
+
+router.start(App, '#app')
