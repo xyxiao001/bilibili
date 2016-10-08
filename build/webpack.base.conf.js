361410bebb37b8f1b17274b8cea9d762b9a94a9a
xyxiao <463618622@qq.com>
2016-10-08 10:09:17 +0800

    去掉图片hash后缀

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -64,7 +64,7 @@ module.exports = {
         loader: 'url',
         query: {
           limit: 10000,
-          name: utils.assetsPath('img/[name].[hash:7].[ext]')
+          name: utils.assetsPath('img/[name].[ext]')
         }
       },
       {
